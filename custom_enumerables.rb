c6ab5ad9e1243d455970cb6d8fcd859124bccb2e
Mindy Johnson <mindy.l.brock@gmail.com>
2021-09-27 19:43:12 -0500

    Created a custom enumerable for 'each_with_index'

--- custom_enumerables.rb
+++ custom_enumerables.rb
@@ -1,7 +1,17 @@
 module Enumerable
   def my_each
+    return to_enum(:my_each) unless block_given?
+
     for item in self
       yield item
+    end
+  end
+
+  def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+
+    for index in 0.. self.size - 1
+      yield self[index], index
     end
   end
 end
--- main.rb
+++ main.rb
@@ -1,8 +1,15 @@
 require_relative 'custom_enumerables'
 
 puts "my_each vs. each"
-numbers = [1, 2, 3, 4, 5]
+numbers = [2, 4, 6, 8, 10]
 puts "\nmy_each"
 numbers.my_each  { |item| puts item }
 puts "\neach"
 numbers.each  { |item| puts item }
+
+puts "\nmy_each_with_index vs. each_with_index"
+numbers = [2, 4, 6, 8, 10]
+puts "\nmy_each_with_index"
+numbers.my_each_with_index  { |item, index| puts "#{item}, #{index}" }
+puts "\neach_with_index"
+numbers.each_with_index  { |item, index| puts "#{item}, #{index}" }
